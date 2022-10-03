41be2bb97d65b99df269c3bda59c4bdb922ad2fa
Jordan Parviainen <jordan@smartdust.me>
2022-10-03 14:02:53 +0200

    Added skeleton for Firefox testing

--- src/test/java/SeleniumAndroidBrowserExampleTest.java
+++ src/test/java/SeleniumAndroidBrowserExampleTest.java
@@ -1,10 +1,21 @@
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-public class SeleniumAndroidBrowserExampleTest extends SeleniumAndroidBrowserTestBase{
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class SeleniumAndroidBrowserExampleTest extends SeleniumAndroidBrowserTestBase {
 
     @Test
-    public void testWebsiteOpen(){
+    public void testWebsiteOpen() {
         driver.get("https://google.com");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until((ExpectedCondition<Boolean>) wdriver ->
+                ((JavascriptExecutor) driver).executeScript("return document.readyState")
+                        .equals("complete"));
     }
-
 }
--- src/test/java/SeleniumAndroidBrowserTestBase.java
+++ src/test/java/SeleniumAndroidBrowserTestBase.java
@@ -1,23 +1,27 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.io.TemporaryFilesystem;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
 public class SeleniumAndroidBrowserTestBase {
-    protected ChromeDriver driver;
+    protected WebDriver driver;
 
     @BeforeTest
     @Parameters({"deviceID", "browser"})
     public void setUp(String deviceID, String browser) {
         ChromeOptions chromeOptions = new ChromeOptions();
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
         switch(browser){
             case "chrome":
                 WebDriverManager.chromedriver().setup();
                 chromeOptions.setExperimentalOption("androidPackage", "com.android.chrome");
+                chromeOptions.setExperimentalOption("androidDeviceSerial", deviceID);
+                driver = new ChromeDriver(chromeOptions);
                 break;
             case "samsung":
                 System.setProperty("webdriver.chrome.driver","C:\\Users\\admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
@@ -26,14 +30,22 @@ public class SeleniumAndroidBrowserTestBase {
                 chromeOptions.setExperimentalOption("androidActivity", "com.sec.android.app.sbrowser.SBrowserMainActivity");
                 chromeOptions.setExperimentalOption("androidDeviceSocket", "Terrace_devtools_remote");
                 chromeOptions.setExperimentalOption("androidExecName", "Terrace");
+                chromeOptions.setExperimentalOption("androidDeviceSerial", deviceID);
+                driver = new ChromeDriver(chromeOptions);
                 break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                firefoxOptions.setAndroidPackage("org.mozilla.firefox");
+                firefoxOptions.setAndroidDeviceSerialNumber(deviceID);
+                driver = new FirefoxDriver(firefoxOptions);
         }
-        chromeOptions.setExperimentalOption("androidDeviceSerial", deviceID);
-        driver = new ChromeDriver(chromeOptions);
     }
 
     @AfterTest
     public void tearDown(){
-        driver.quit();
+        if(driver instanceof FirefoxDriver)
+            driver.close();
+        else
+            driver.quit();
     }
 }
