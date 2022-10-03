78918259cf74ce32797db2422cc21016c52b3b8c
Jordan Parviainen <jordan@smartdust.me>
2022-10-03 11:29:57 +0200

    Added Samsung Internet Browser setup and parametrized browser in tests

--- src/test/java/SeleniumAndroidBrowserTestBase.java
+++ src/test/java/SeleniumAndroidBrowserTestBase.java
@@ -1,24 +1,33 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.io.TemporaryFilesystem;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
 public class SeleniumAndroidBrowserTestBase {
-
     protected ChromeDriver driver;
 
-    @BeforeSuite
-    public void setUpDriver(){
-        WebDriverManager.chromedriver().setup();
-    }
     @BeforeTest
-    @Parameters("deviceID")
-    public void setUp(String deviceID) {
+    @Parameters({"deviceID", "browser"})
+    public void setUp(String deviceID, String browser) {
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setExperimentalOption("androidPackage", "com.android.chrome");
+        switch(browser){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                chromeOptions.setExperimentalOption("androidPackage", "com.android.chrome");
+                break;
+            case "samsung":
+                System.setProperty("webdriver.chrome.driver","C:\\Users\\admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+                //this needs to be set to the path of the downloaded correct version of the chromedriver
+                chromeOptions.setExperimentalOption("androidPackage", "com.sec.android.app.sbrowser");
+                chromeOptions.setExperimentalOption("androidActivity", "com.sec.android.app.sbrowser.SBrowserMainActivity");
+                chromeOptions.setExperimentalOption("androidDeviceSocket", "Terrace_devtools_remote");
+                chromeOptions.setExperimentalOption("androidExecName", "Terrace");
+                break;
+        }
         chromeOptions.setExperimentalOption("androidDeviceSerial", deviceID);
         driver = new ChromeDriver(chromeOptions);
     }
